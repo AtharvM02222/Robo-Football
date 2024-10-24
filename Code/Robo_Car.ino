ad29a8ff973d57e05874587b95072ee97f5929de
Atharv M <112397185+AtharvM02222@users.noreply.github.com>
2024-10-24 08:40:16 +0530

    Update Robo_Car.ino

--- Code/Robo_Car.ino
+++ Code/Robo_Car.ino
@@ -1,171 +1,163 @@
-#include "arduino_secrets.h"
 
-#include <AFMotor.h>
-  
-//initial motors pin
-AF_DCMotor motor1(1, MOTOR12_1KHZ);
-AF_DCMotor motor2(2, MOTOR12_1KHZ);
-AF_DCMotor motor3(3, MOTOR34_1KHZ);
-AF_DCMotor motor4(4, MOTOR34_1KHZ);
-  
-int val;
-int Speeed = 255;
-  
-void setup()
-{
+#define in1 5 //L298n Motor Driver pins.
+#define in2 6
+#define in3 10
+#define in4 11
+#define LED 13
+int command; //Int to store app command state.
+int Speed = 204; // 0 - 255.
+int Speedsec;
+int buttonState = 0;
+int lastButtonState = 0;
+int Turnradius = 0; //Set the radius of a turn, 0 - 255 Note:the robot will malfunction if this is higher than int Speed.
+int brakeTime = 45;
+int brkonoff = 1; //1 for the electronic braking system, 0 for normal.
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(LED, OUTPUT); //Set the LED pin.
   Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
 }
-void loop(){
-  if(Serial.available() > 0){
-    val = Serial.read();
-      
-    Stop(); //initialize with motors stoped
-      
-          if (val == 'F'){
-          forward();
-          }
-  
-          if (val == 'B'){
-          back();
-          }
-  
-          if (val == 'L'){
-          left();
-          }
-  
-          if (val == 'R'){
-          right();
-          }
-          if (val == 'I'){
-          topright();
-          }
-  
-          if (val == 'J'){
-          topleft();
-          }
-  
-          if (val == 'K'){
-          bottomright();
-          }
-  
-          if (val == 'M'){
-          bottomleft();
-          }
-          if (val == 'T'){
-          Stop();
-          }
+
+void loop() {
+  if (Serial.available() > 0) {
+    command = Serial.read();
+    Stop(); //Initialize with motors stoped.
+    switch (command) {
+      case 'F':
+        forward();
+        break;
+      case 'B':
+        back();
+        break;
+      case 'L':
+        left();
+        break;
+      case 'R':
+        right();
+        break;
+      case 'G':
+        forwardleft();
+        break;
+      case 'I':
+        forwardright();
+        break;
+      case 'H':
+        backleft();
+        break;
+      case 'J':
+        backright();
+        break;
+      case '0':
+        Speed = 100;
+        break;
+      case '1':
+        Speed = 140;
+        break;
+      case '2':
+        Speed = 153;
+        break;
+      case '3':
+        Speed = 165;
+        break;
+      case '4':
+        Speed = 178;
+        break;
+      case '5':
+        Speed = 191;
+        break;
+      case '6':
+        Speed = 204;
+        break;
+      case '7':
+        Speed = 216;
+        break;
+      case '8':
+        Speed = 229;
+        break;
+      case '9':
+        Speed = 242;
+        break;
+      case 'q':
+        Speed = 255;
+        break;
+    }
+    Speedsec = Turnradius;
+    if (brkonoff == 1) {
+      brakeOn();
+    } else {
+      brakeOff();
+    }
   }
 }
-           
-  
-            
-  
-  
-  
-void forward()
-{
-  motor1.setSpeed(Speeed); //Define maximum velocity
-  motor1.run(FORWARD); //rotate the motor clockwise
-  motor2.setSpeed(Speeed); //Define maximum velocity
-  motor2.run(FORWARD); //rotate the motor clockwise
-  motor3.setSpeed(Speeed);//Define maximum velocity
-  motor3.run(FORWARD); //rotate the motor clockwise
-  motor4.setSpeed(Speeed);//Define maximum velocity
-  motor4.run(FORWARD); //rotate the motor clockwise
+
+void forward() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speed);
 }
-  
-void back()
-{
-  motor1.setSpeed(Speeed); //Define maximum velocity
-  motor1.run(BACKWARD); //rotate the motor anti-clockwise
-  motor2.setSpeed(Speeed); //Define maximum velocity
-  motor2.run(BACKWARD); //rotate the motor anti-clockwise
-  motor3.setSpeed(Speeed); //Define maximum velocity
-  motor3.run(BACKWARD); //rotate the motor anti-clockwise
-  motor4.setSpeed(Speeed); //Define maximum velocity
-  motor4.run(BACKWARD); //rotate the motor anti-clockwise
+
+void back() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speed);
 }
-  
-void left()
-{
-  motor1.setSpeed(Speeed); //Define maximum velocity
-  motor1.run(BACKWARD); //rotate the motor anti-clockwise
-  motor2.setSpeed(Speeed); //Define maximum velocity
-  motor2.run(BACKWARD); //rotate the motor anti-clockwise
-  motor3.setSpeed(Speeed); //Define maximum velocity
-  motor3.run(FORWARD);  //rotate the motor clockwise
-  motor4.setSpeed(Speeed); //Define maximum velocity
-  motor4.run(FORWARD);  //rotate the motor clockwise
+
+void left() {
+  analogWrite(in3, Speed);
+  analogWrite(in2, Speed);
 }
-  
-void right()
-{
-  motor1.setSpeed(Speeed); //Define maximum velocity
-  motor1.run(FORWARD); //rotate the motor clockwise
-  motor2.setSpeed(Speeed); //Define maximum velocity
-  motor2.run(FORWARD); //rotate the motor clockwise
-  motor3.setSpeed(Speeed); //Define maximum velocity
-  motor3.run(BACKWARD); //rotate the motor anti-clockwise
-  motor4.setSpeed(Speeed); //Define maximum velocity
-  motor4.run(BACKWARD); //rotate the motor anti-clockwise
+
+void right() {
+  analogWrite(in4, Speed);
+  analogWrite(in1, Speed);
 }
-  
-void topleft(){
-  motor1.setSpeed(Speeed); //Define maximum velocity
-  motor1.run(FORWARD); //rotate the motor clockwise
-  motor2.setSpeed(Speeed); //Define maximum velocity
-  motor2.run(FORWARD); //rotate the motor clockwise
-  motor3.setSpeed(Speeed/3.1);//Define maximum velocity
-  motor3.run(FORWARD); //rotate the motor clockwise
-  motor4.setSpeed(Speeed/3.1);//Define maximum velocity
-  motor4.run(FORWARD); //rotate the motor clockwise
+void forwardleft() {
+  analogWrite(in1, Speedsec);
+  analogWrite(in3, Speed);
 }
-  
-void topright()
-{
-  motor1.setSpeed(Speeed/3.1); //Define maximum velocity
-  motor1.run(FORWARD); //rotate the motor clockwise
-  motor2.setSpeed(Speeed/3.1); //Define maximum velocity
-  motor2.run(FORWARD); //rotate the motor clockwise
-  motor3.setSpeed(Speeed);//Define maximum velocity
-  motor3.run(FORWARD); //rotate the motor clockwise
-  motor4.setSpeed(Speeed);//Define maximum velocity
-  motor4.run(FORWARD); //rotate the motor clockwise
+void forwardright() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speedsec);
 }
-  
-void bottomleft()
-{
-  motor1.setSpeed(Speeed); //Define maximum velocity
-  motor1.run(BACKWARD); //rotate the motor anti-clockwise
-  motor2.setSpeed(Speeed); //Define maximum velocity
-  motor2.run(BACKWARD); //rotate the motor anti-clockwise
-  motor3.setSpeed(Speeed/3.1); //Define maximum velocity
-  motor3.run(BACKWARD); //rotate the motor anti-clockwise
-  motor4.setSpeed(Speeed/3.1); //Define maximum velocity
-  motor4.run(BACKWARD); //rotate the motor anti-clockwise
+void backright() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speedsec);
 }
-  
-void bottomright()
-{
-  motor1.setSpeed(Speeed/3.1); //Define maximum velocity
-  motor1.run(BACKWARD); //rotate the motor anti-clockwise
-  motor2.setSpeed(Speeed/3.1); //Define maximum velocity
-  motor2.run(BACKWARD); //rotate the motor anti-clockwise
-  motor3.setSpeed(Speeed); //Define maximum velocity
-  motor3.run(BACKWARD); //rotate the motor anti-clockwise
-  motor4.setSpeed(Speeed); //Define maximum velocity
-  motor4.run(BACKWARD); //rotate the motor anti-clockwise
+void backleft() {
+  analogWrite(in2, Speedsec);
+  analogWrite(in4, Speed);
 }
-  
-  
-void Stop()
-{
-  motor1.setSpeed(0); //Define minimum velocity
-  motor1.run(RELEASE); //stop the motor when release the button
-  motor2.setSpeed(0); //Define minimum velocity
-  motor2.run(RELEASE); //rotate the motor clockwise
-  motor3.setSpeed(0); //Define minimum velocity
-  motor3.run(RELEASE); //stop the motor when release the button
-  motor4.setSpeed(0); //Define minimum velocity
-  motor4.run(RELEASE); //stop the motor when release the button
+
+void Stop() {
+  analogWrite(in1, 0);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
+  analogWrite(in4, 0);
+}
+
+void brakeOn() {
+  //Here's the future use: an electronic braking system!
+  // read the pushbutton input pin:
+  buttonState = command;
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == 'S') {
+      if (lastButtonState != buttonState) {
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, HIGH);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, HIGH);
+        delay(brakeTime);
+        Stop();
+      }
+    }
+    // save the current state as the last state,
+    //for next time through the loop
+    lastButtonState = buttonState;
+  }
+}
+void brakeOff() {
+
 }
