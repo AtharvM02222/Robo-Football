6cc6c9f1c599c688d94a6bbadb2a97fc15a5b538
Atharv M <112397185+AtharvM02222@users.noreply.github.com>
2024-10-31 11:05:21 +0530

    Update Robo_Car.ino

--- Code/Robo_Car.ino
+++ Code/Robo_Car.ino
@@ -1,131 +1,165 @@
-#define in1 5 //L298n Motor Driver pins.
-#define in2 6
-#define in3 10
+#define in1 3 // L298N Motor Driver pins
+#define in2 5
+#define in3 6
 #define in4 11
-#define LED 13
-int command; //Int to store app command state.
-int Speed = 204; // 0 - 255.
-int Speedsec;
-int buttonState = 0;
-int lastButtonState = 0;
-int Turnradius = 0; //Set the radius of a turn, 0 - 255 Note:the robot will malfunction if this is higher than int Speed.
-int brakeTime = 45;
-int brkonoff = 1; //1 for the electronic braking system, 0 for normal.
+
+int command = 'S'; // Default command is Stop
+int Speed = 100; // Initial speed (range: 0-255)
+unsigned long lastCommandTime = 0; // Time of the last command
+const unsigned long timeout = 100; // Timeout period in milliseconds
+
 void setup() {
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  pinMode(LED, OUTPUT); //Set the LED pin.
-  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+  Serial.begin(9600); // Set the baud rate for Bluetooth communication
 }
 
 void loop() {
+  bool newCommand = false;
+
+  // Check if a command is available
   if (Serial.available() > 0) {
     command = Serial.read();
-    Stop(); //Initialize with motors stoped.
-    switch (command) {
-      case 'F':
-        forward();
-        break;
-      case 'B':
-        back();
-        break;
-      case 'L':
-        left();
-        break;
-      case 'R':
-        right();
-        break;
-      case 'G':
-        forwardleft();
-        break;
-      case 'I':
-        forwardright();
-        break;
-      case 'H':
-        backleft();
-        break;
-      case 'J':
-        backright();
-        break;
-      case '0':
-        Speed = 100;
-        break;
-      case '1':
-        Speed = 140;
-        break;
-      case '2':
-        Speed = 153;
-        break;
-      case '3':
-        Speed = 165;
-        break;
-      case '4':
-        Speed = 178;
-        break;
-      case '5':
-        Speed = 191;
-        break;
-      case '6':
-        Speed = 204;
-        break;
-      case '7':
-        Speed = 216;
-        break;
-      case '8':
-        Speed = 229;
-        break;
-      case '9':
-        Speed = 242;
-        break;
-      case 'q':
-        Speed = 255;
-        break;
-    }
-    Speedsec = Turnradius;
-    if (brkonoff == 1) {
-      brakeOn();
-    } else {
-      brakeOff();
-    }
+    lastCommandTime = millis(); // Update the time of the last command
+    newCommand = true; // A new command is received
+  }
+
+  // Execute the command only if it's new
+  if (newCommand) {
+    handleCommand(command);
+  }
+
+  // Stop the car if the timeout has passed since the last command
+  if (millis() - lastCommandTime > timeout) {
+    Stop();
+  }
+
+  // Reset command after each loop to prevent it from sticking
+  command = 'S';
+}
+
+void handleCommand(char cmd) {
+  switch (cmd) {
+    case 'F':
+      forward();
+      break;
+    case 'B':
+      back();
+      break;
+    case 'L':
+      left();
+      break;
+    case 'R':
+      right();
+      break;
+    case 'G':
+      forwardleft();
+      break;
+    case 'I':
+      forwardright();
+      break;
+    case 'H':
+      backleft();
+      break;
+    case 'J':
+      backright();
+      break;
+    case 'S': // Stop the robot
+    case 'D': // Stop all movements
+      Stop();
+      break;
+    case '0':
+      Speed = 0;
+      break;
+    case '1':
+      Speed = 10;
+      break;
+    case '2':
+      Speed = 20;
+      break;
+    case '3':
+      Speed = 30;
+      break;
+    case '4':
+      Speed = 40;
+      break;
+    case '5':
+      Speed = 50;
+      break;
+    case '6':
+      Speed = 60;
+      break;
+    case '7':
+      Speed = 70;
+      break;
+    case '8':
+      Speed = 80;
+      break;
+    case '9':
+      Speed = 90;
+      break;
+    case 'q':
+      Speed = 100;
+      break;
   }
 }
 
 void forward() {
   analogWrite(in1, Speed);
   analogWrite(in3, Speed);
+  analogWrite(in2, 0);
+  analogWrite(in4, 0);
 }
 
 void back() {
   analogWrite(in2, Speed);
   analogWrite(in4, Speed);
+  analogWrite(in1, 0);
+  analogWrite(in3, 0);
 }
 
 void left() {
   analogWrite(in3, Speed);
   analogWrite(in2, Speed);
+  analogWrite(in1, 0);
+  analogWrite(in4, 0);
 }
 
 void right() {
   analogWrite(in4, Speed);
   analogWrite(in1, Speed);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
 }
+
 void forwardleft() {
-  analogWrite(in1, Speedsec);
+  analogWrite(in1, Speed * 0.7);
   analogWrite(in3, Speed);
+  analogWrite(in2, 0);
+  analogWrite(in4, 0);
 }
+
 void forwardright() {
   analogWrite(in1, Speed);
-  analogWrite(in3, Speedsec);
+  analogWrite(in3, Speed * 0.7);
+  analogWrite(in2, 0);
+  analogWrite(in4, 0);
 }
+
+void backleft() {
+  analogWrite(in2, Speed * 0.7);
+  analogWrite(in4, Speed);
+  analogWrite(in1, 0);
+  analogWrite(in3, 0);
+}
+
 void backright() {
   analogWrite(in2, Speed);
-  analogWrite(in4, Speedsec);
-}
-void backleft() {
-  analogWrite(in2, Speedsec);
-  analogWrite(in4, Speed);
+  analogWrite(in4, Speed * 0.7);
+  analogWrite(in1, 0);
+  analogWrite(in3, 0);
 }
 
 void Stop() {
@@ -133,30 +167,4 @@ void Stop() {
   analogWrite(in2, 0);
   analogWrite(in3, 0);
   analogWrite(in4, 0);
-}
-
-void brakeOn() {
-  //Here's the future use: an electronic braking system!
-  // read the pushbutton input pin:
-  buttonState = command;
-  // compare the buttonState to its previous state
-  if (buttonState != lastButtonState) {
-    // if the state has changed, increment the counter
-    if (buttonState == 'S') {
-      if (lastButtonState != buttonState) {
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, HIGH);
-        delay(brakeTime);
-        Stop();
-      }
-    }
-    // save the current state as the last state,
-    //for next time through the loop
-    lastButtonState = buttonState;
-  }
-}
-void brakeOff() {
-
 }
